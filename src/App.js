5a6fa8b183a4d6b18f3fefd5f371ecf08bc017b8
root <root@DESKTOP-LLPQSLM.localdomain>
2021-03-02 10:55:05 -0500

    created empty files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
+
+const fetchBoard = async () => {
+  const data = await fetch(`https://api.trello.com/1/actions/{id}/board?key=0471642aefef5fa1fa76530ce1ba4c85&token=9eb76d9a9d02b8dd40c2f3e5df18556c831d4d1fadbe2c45f8310e6c93b5c548`);
+  console.log("data",data);
+}
+
+fetchBoard();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+
+
+function Header() {
+  return (
+    <div className="">
+       iconos
+    </div>
+  );
+}
+
+export default Header;
