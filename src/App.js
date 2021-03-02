3ed58428f9a486c0337bc6a83b36015a75f745c8
root <root@DESKTOP-LLPQSLM.localdomain>
2021-03-02 11:24:21 -0500

    add trello key

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Header from "./components/Header";
+import TrelloClient from "react-trello-client";
+
 
 function App() {
 
@@ -11,7 +13,29 @@ const fetchBoard = async () => {
 fetchBoard();
   return (
     <div className="App">
-        <Header />
+      <Header />
+      <TrelloClient
+            apiKey="2d6549ffae8065bc4354b8abcb14a28e" // Get the API key from https://trello.com/app-key/
+            clientVersion={1} // number: {1}, {2}, {3}
+            apiEndpoint="https://api.trello.com" // string: "https://api.trello.com"
+            authEndpoint="https://trello.com" // string: "https://trello.com"
+            intentEndpoint="https://trello.com" // string: "https://trello.com"
+            authorizeName="React Trello Client" // string: "React Trello Client"
+            authorizeType="popup" // string: popup | redirect
+            authorizePersist={true}
+            authorizeInteractive={true}
+            authorizeScopeRead={false} // boolean: {true} | {false}
+            authorizeScopeWrite={true} // boolean: {true} | {false}
+            authorizeScopeAccount={true} // boolean: {true} | {false}
+            authorizeExpiration="never" // string: "1hour", "1day", "30days" | "never"
+            authorizeOnSuccess={() => console.log('Login successful!')} // function: {() => console.log('Login successful!')}
+            authorizeOnError={() => console.log('Login error!')} // function: {() => console.log('Login error!')}
+            autoAuthorize={false} // boolean: {true} | {false}
+            authorizeButton={true} // boolean: {true} | {false}
+            buttonStyle="metamorph" // string: "metamorph" | "flat"
+            buttonColor="green" // string: "green" | "grayish-blue" | "light"
+            buttonText="Login with Trello" // string: "Login with Trello"
+        />
     </div>
   );
 }
