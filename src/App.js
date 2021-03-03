b026305561dbc508d9b0462c8684f7080b637e30
susanluisa <susanluisacoari@gmail.com>
2021-03-02 23:51:16 -0500

    component Board created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { React } from 'react';
 import Main from './components/Main';
 import Header from "./components/Header";
 import './App.css';
-import TrelloClient from "react-trello-client";
 
 function App() {
 
@@ -10,28 +9,6 @@ function App() {
     <div className="App">
       <Header />
       <Main />
-      {/* <TrelloClient
-            apiKey="2d6549ffae8065bc4354b8abcb14a28e" // Get the API key from https://trello.com/app-key/
-            clientVersion={1} // number: {1}, {2}, {3}
-            apiEndpoint="https://api.trello.com" // string: "https://api.trello.com"
-            authEndpoint="https://trello.com" // string: "https://trello.com"
-            intentEndpoint="https://trello.com" // string: "https://trello.com"
-            authorizeName="React Trello Client" // string: "React Trello Client"
-            authorizeType="popup" // string: popup | redirect
-            authorizePersist={true}
-            authorizeInteractive={true}
-            authorizeScopeRead={false} // boolean: {true} | {false}
-            authorizeScopeWrite={true} // boolean: {true} | {false}
-            authorizeScopeAccount={true} // boolean: {true} | {false}
-            authorizeExpiration="never" // string: "1hour", "1day", "30days" | "never"
-            authorizeOnSuccess={() => console.log('Login successful!')} // function: {() => console.log('Login successful!')}
-            authorizeOnError={() => console.log('Login error!')} // function: {() => console.log('Login error!')}
-            autoAuthorize={false} // boolean: {true} | {false}
-            authorizeButton={true} // boolean: {true} | {false}
-            buttonStyle="metamorph" // string: "metamorph" | "flat"
-            buttonColor="green" // string: "green" | "grayish-blue" | "light"
-            buttonText="Login with Trello" // string: "Login with Trello"
-        /> */}
     </div>
   );
 }
--- src/components/Boards.js
+++ src/components/Boards.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import './Main.css';
+import './Boards.css';
 
 function Boards() {
 
@@ -7,23 +7,23 @@ function Boards() {
     fetchBoard();
   }, []);
 
-  const [board, setBoard] = useState([]);
+  const [boards, setBoards] = useState({});
   const fetchBoard = async () => {
     const data = await fetch(`https://api.trello.com/1/members/me/boards?fields=name,url&key=2d6549ffae8065bc4354b8abcb14a28e&token=687e0ea9237de53d5fe2aa084efefa2c77e10c4c2fdbcde2a7d2d285e6c56309`);
     const boards = await data.json();
-    console.log(boards[0].name);
-
-    // boards.forEach(board => {
-    //   console.log(board);
-    // });
+    boards.forEach(board => {
+      setBoards(board);
+    });
   }
-
-  return (
-    <div className="">
-      <div className="">
-      </div>
-    </div>
-  );
+  console.log(boards);
+  const title = boards.forEach(board=>{
+    return(
+      <div className="board__content">
+        <h2>{board.name}</h2>
+      </div>)
+  })
+ 
+  return (title);
 }
 
 export default Boards;
